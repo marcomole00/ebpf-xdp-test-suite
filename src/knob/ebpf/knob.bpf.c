5bb7a1b067063efae81c041b178d74ed8c02b33f
marcomole00 <marcomole00@gmail.com>
2024-12-30 11:28:12 -0600

    fix: knob now inserts

--- src/knob/ebpf/knob.bpf.c
+++ src/knob/ebpf/knob.bpf.c
@@ -7,18 +7,19 @@
 // nr_loops is limited to 1 << 23 (~8 million) loops
 #define NR_LOOPS 1<<15
 
-long important_computation (__u32 index, void *ctx){
+static long important_computation (__u32 index, void *ctx){
   int random_value = bpf_get_prandom_u32();
-  struct xdp_md *xdp = (struct xdp_md *) ctx;
-  xdp->data += random_value;  
+  int *number = (int *) ctx;
+  *number+= random_value;
   return 0;
 }
 
 
 SEC("xdp_pass")
 int xdp_pass_func(struct xdp_md *ctx) {
-  bpf_loop(NR_LOOPS, important_computation, ctx, 0);
-   return XDP_DROP ;
+  int number = 0;
+  bpf_loop(NR_LOOPS, important_computation, &number, 0);
+  return XDP_DROP;
 }
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
